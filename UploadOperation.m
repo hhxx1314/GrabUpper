23b63e3c571040069a4a08590b87229bffecacea
Erica Sadun <ericasadun@Banana.local>
2009-09-07 13:56:44 -0600

    Cleaned up code, commented more liberally.

--- UploadOperation.m
+++ UploadOperation.m
@@ -9,7 +9,7 @@
 #define NOTIFY_AND_LEAVE(X) {[self cleanup:X]; return;}
 #define DATA(X)	[X dataUsingEncoding:NSUTF8StringEncoding]
 
-// Posting constants
+// Posting constants. Defined as constants for historical reasons (reused code)
 #define BOUNDARY @"------------0x0x0x0x0x0x0x0x"
 #define MULTIPART @"multipart/form-data; boundary=------------0x0x0x0x0x0x0x0x"
 #define PREFIX [NSString stringWithFormat:@"--%@\r\n", BOUNDARY]
@@ -21,6 +21,7 @@
 @synthesize path;
 @synthesize delegate;
 
+// On cleanup, just re-direct the string to the delegate
 - (void) cleanup: (NSString *) output
 {
 	if (self.delegate && [self.delegate respondsToSelector:@selector(doneUploading:)])
@@ -29,14 +30,17 @@
 
 - (void) main
 {
+	// Create the mutable  body data
 	NSMutableData *postData = [[NSMutableData alloc] init];
 
+	// First of two arguments, action to take
 	NSString *firstArg = [NSString stringWithFormat:STRING_CONTENT, @"app_upload"];
 	[postData appendData:DATA(PREFIX)];
 	[postData appendData:DATA(firstArg)];
 	[postData appendData:DATA(@"Upload")];
 	[postData appendData:DATA(EACH_SUFFIX)];
 	
+	// Second of two arguments, the file name and data
 	NSString *secondArg = [NSString stringWithFormat:@"Content-Disposition: form-data; name=\"userfile\"; filename=\"%@\"\r\n", [self.path lastPathComponent]];
 	[postData appendData:DATA(PREFIX)];
 	[postData appendData:DATA(secondArg)];
@@ -44,14 +48,14 @@
 	[postData appendData:[NSData dataWithContentsOfFile:self.path]];
 	[postData appendData:DATA(EACH_SUFFIX)];
 	
+	// Finish form
 	[postData appendData:DATA(SUFFIX)];
 	
-	
+	// Create the POST URL request
     NSString *baseurl = @"http://www.grabup.com/app_upload.php"; 
     NSURL *url = [NSURL URLWithString:baseurl];
     NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:url];
     if (!urlRequest) NOTIFY_AND_LEAVE(@"Error creating the URL Request");
-	
     [urlRequest setHTTPMethod: @"POST"];
 	[urlRequest setValue:MULTIPART forHTTPHeaderField: @"Content-Type"];
     [urlRequest setHTTPBody:postData];
--- grabupper.m
+++ grabupper.m
@@ -8,23 +8,36 @@
 @implementation Watcher
 - (void) doneUploading:(NSString *)status
 {
-	// Range didn't work. This does.
+	// Range didn't work. This does.  First look for imgup
 	NSString *endString = [[status componentsSeparatedByString:@"imgup"] lastObject];
+	
+	// Then grab the start of the http://
 	NSArray *items = [endString componentsSeparatedByString:@"img src=\""];
 	if (items.count < 2) {printf("Error extracting URL from GrabUp response.");	return;}
 	
+	// Find the end of the URL
 	endString = [items objectAtIndex:1];
 	items = [endString componentsSeparatedByString:@"?direct"];
 	if (items.count < 2) {printf("Error extracting URL from GrabUp response.");	return;}
 	
+	// And add the ?direct back in. (Trust me, it's better that way.)
 	endString = [[items objectAtIndex:0] stringByAppendingString:@"?direct"];
+	
+	// Create URL from the string
 	NSURL *url = [NSURL URLWithString:endString];
+	
+	// Add it to the pasteboard
 	NSString *type = NSURLPboardType;
 	[[NSPasteboard generalPasteboard] declareTypes:[NSArray arrayWithObject:type] owner:nil];
 	[url writeToPasteboard:[NSPasteboard generalPasteboard]];
 	
-	printf("Pasteboard: %s\n", [endString UTF8String]);
-	system("say 'You Are Ell is ready'");
+	// Write info to stdout too
+	printf("Pasteboard:\n%s\n\n", [endString UTF8String]);
+	
+	// Alert with result. Select from any of the following.
+	system("afplay /System/Library/Sounds/Glass.aiff");
+	// system("say 'You Are Ell is ready'");
+
 }
 
 - (void) notify: (NSNotification *) notification
@@ -41,25 +54,33 @@
 	NSString *basepath = [NSHomeDirectory() stringByAppendingPathComponent:@"Desktop"];
 	if (scdict && [scdict objectForKey:@"location"]) basepath = [scdict objectForKey:@"location"];
 		
-	
+	// Iterate through all files at that path
 	NSArray *files = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:basepath error:nil];
 	for (NSString *fileName in files)
 	{
+		// if there's a file that matches the prefix
 		if ([fileName hasPrefix:prefix])
 		{
+			// Extract the date string and use natural language matching to get its date
 			NSString *datestring = [[[[[fileName stringByDeletingPathExtension] substringFromIndex:8] stringByReplacingOccurrencesOfString:@" at" withString:@""] stringByReplacingOccurrencesOfString:@"." withString:@":"] stringByAppendingString:@" -0600"];
 			NSDate *picDate = [NSDate dateWithNaturalLanguageString:datestring];
+			
+			// Determine the length of time since the screen was shot.
 			NSTimeInterval t = [[NSDate date] timeIntervalSinceDate:picDate];
+			
+			// Proceed if the time interval lies within 5 seconds
 			if (t < 5.0f)
 			{
-				CFShow(fileName);
+				// Get the full path and the actual image
 				NSString *path = [basepath stringByAppendingPathComponent:fileName];
-				
 				NSImage *image = [[[NSImage alloc]  initWithContentsOfFile:path] autorelease];
+				
+				// Convert the image to jpeg 
 				NSArray *representations = [image representations];
 				NSData * bitmapData = [NSBitmapImageRep representationOfImageRepsInArray:representations usingType:NSJPEGFileType properties:nil];
 				[bitmapData writeToFile:@"/tmp/foo.jpg" atomically:YES];
 				
+				// Upload the image
 				UploadOperation *op = [[[UploadOperation alloc] init] autorelease];
 				op.path = @"/tmp/foo.jpg";
 				op.delegate = self;
@@ -72,8 +93,9 @@
 
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-    printf("Starting Pic Capture Scan. ^C to quit.\n");
 	
+	// Listen for Directory Update events
+    printf("Starting Pic Capture Scan. ^C to quit.\n");
 	Watcher *watcher = [[Watcher alloc] init];
 	[[NSDistributedNotificationCenter defaultCenter] addObserver:watcher selector:@selector(notify:) name:@"com.apple.carbon.core.DirectoryNotification" object:nil];
 	CFRunLoopRun();
